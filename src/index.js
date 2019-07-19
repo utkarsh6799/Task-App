b9bb616ed0446c4ddaf977876beb1f3807b4e001
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-19 22:13:02 +0530

    validation file support

--- src/index.js
+++ src/index.js
@@ -9,7 +9,24 @@ const port = process.env.PORT || 3000
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)){
+            return cb(new Error('Please upload a word document'))
+        }
+        
+        cb(undefined, true)
+
+        // cb(new Error('File must be a pdf'))
+        // cb(undefined, true)
+        // cb(undefined, false)
+        
+
+
+    }
 })
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
--- src/routers/users.js
+++ src/routers/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const Users = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
+
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -101,13 +102,24 @@ router.delete('/users/me', auth, async (req, res) => {
 
 
     })
-
+     
     const upload = multer({
-        dest: 'avatars'
+        dest: 'avatars',
+        limits: {
+            fileSize: 1000000
+        },
+        fileFilter(req, file, cb) {
+            if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+                return cb(new Error('Please upload an image'))
+            }
+    
+            cb(undefined, true)
+        }
     })
+    
 
     router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
-         res.send()
+        res.send()
     })
 
 
