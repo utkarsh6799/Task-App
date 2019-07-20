f4aae7320cf8a92c044a5de689f969552d30f071
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-07-20 13:02:27 +0530

    cropping and formatting image

--- src/routers/users.js
+++ src/routers/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const Users = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
+const sharp = require('sharp')
 
 const router = new express.Router()
 
@@ -118,13 +119,16 @@ router.delete('/users/me', auth, async (req, res) => {
     })
    
     router.post('/users/me/avatar', auth, upload.single('avatar') , async (req, res) => {
-        req.user.avatar = req.file.buffer
+       const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+        
+
+        req.user.avatar = buffer
         await req.user.save()
         res.send()
     }, (error, req, res, next) => {
        res.status(400).send({ error: error.message })
     })
-    
+
 
     router.delete('/users/me/avatar', auth, async (req, res) => {
         req.user.avatar = undefined
@@ -141,7 +145,7 @@ router.delete('/users/me', auth, async (req, res) => {
 
           }
 
-          res.set('Content-Type', 'image/jpg')
+          res.set('Content-Type', 'image/png')
           res.send(user.avatar)
 
         }catch(e){
